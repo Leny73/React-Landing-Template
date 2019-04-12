d65475fc660ecd3471f1659ad85fe74530167cf0
Leny73 <lbranzalov95@gmail.com>
2019-04-12 11:06:33 +0300

    Updates

--- precache-manifest.31eb830ee2b83aaac8cd1f2de64fe2b6.js
+++ precache-manifest.31eb830ee2b83aaac8cd1f2de64fe2b6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b0aa499a3d7ab09c2800890096191881",
-    "url": "/React-Landing-Template/static/media/volkan-olmez-73767-unsplash.b0aa499a.jpg"
+    "revision": "faff2605117360bb3a364dd747b3708f",
+    "url": "/React-Landing-Template/static/media/toa-heftiba-526264-unsplash.faff2605.jpg"
   },
   {
-    "revision": "5d2df10b16d6b6f3d7a0",
-    "url": "/React-Landing-Template/static/css/main.e9dcd033.chunk.css"
+    "revision": "0faf8aa69140ac66dcf7",
+    "url": "/React-Landing-Template/static/css/main.41756bb3.chunk.css"
   },
   {
     "revision": "1aa9f49fd3c2e6be3c90",
@@ -16,20 +16,20 @@ self.__precacheManifest = [
     "url": "/React-Landing-Template/static/js/2.05ce53a0.chunk.js"
   },
   {
-    "revision": "fac394317bd75109559d59ce65a4de0f",
-    "url": "/React-Landing-Template/static/media/header.fac39431.png"
-  },
-  {
     "revision": "54b1edc117ce2c0dc10210da2bf049fd",
     "url": "/React-Landing-Template/static/media/chuttersnap-598266-unsplash.54b1edc1.jpg"
   },
   {
-    "revision": "5d2df10b16d6b6f3d7a0",
+    "revision": "fac394317bd75109559d59ce65a4de0f",
+    "url": "/React-Landing-Template/static/media/header.fac39431.png"
+  },
+  {
+    "revision": "0faf8aa69140ac66dcf7",
     "url": "/React-Landing-Template/static/js/main.a18956ba.chunk.js"
   },
   {
-    "revision": "faff2605117360bb3a364dd747b3708f",
-    "url": "/React-Landing-Template/static/media/toa-heftiba-526264-unsplash.faff2605.jpg"
+    "revision": "b0aa499a3d7ab09c2800890096191881",
+    "url": "/React-Landing-Template/static/media/volkan-olmez-73767-unsplash.b0aa499a.jpg"
   },
   {
     "revision": "1f14dc570a5d148b2c7f75c05aa83b98",
@@ -44,7 +44,7 @@ self.__precacheManifest = [
     "url": "/React-Landing-Template/static/media/rawpixel-196509-unsplash.a8ec8bd2.jpg"
   },
   {
-    "revision": "46ec669b31ea2a58c682522d576dd658",
+    "revision": "207ddc318a67f916a4f98ef62596c34c",
     "url": "/React-Landing-Template/index.html"
   }
 ];
